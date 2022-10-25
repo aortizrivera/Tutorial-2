68a3fdc3f686ea60e2414d33fbd9e7be47b803bc
aortizrivera <oadri187@gmail.com>
2022-10-24 22:01:28 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,22 +7,47 @@ using TMPro;
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
-
     public float speed;
 
     public TextMeshProUGUI score;
+    public TextMeshProUGUI lives;
+
+    public AudioClip backgroundMusic;
+    public AudioClip winMusic;
+    public AudioClip loseMusic;
+    public AudioSource musicSource;
 
     public GameObject winText;
+    public GameObject loseText;
 
     private int scoreValue = 0;
+    private int livesValue = 3;
+
+    private bool gameOver = false;
+
+    private bool facingRight = true;
+
+    private bool isOnGround;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask allGround;
+
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        lives.text = livesValue.ToString();
 
         winText.SetActive(false);
+        loseText.SetActive(false);
+
+        musicSource.clip = backgroundMusic;
+        musicSource.Play();
+
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -32,6 +57,51 @@ public class PlayerScript : MonoBehaviour
         float vertMovement = Input.GetAxis("Vertical");
 
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        {
+            anim.SetInteger("State", 1);
+        }
+        else
+        {
+            anim.SetInteger("State", 0);
+        }
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetInteger("State", 2);
+
+            if (isOnGround == false)
+            {
+
+            }
+            else
+            {
+                anim.SetInteger("State", 0);
+            }
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -43,15 +113,52 @@ public class PlayerScript : MonoBehaviour
             Destroy(collision.collider.gameObject);
         }
 
-        if (scoreValue >= 2)
+        if (scoreValue == 4)
+        {
+            Collider2D collider = collision.collider;
+
+            if(collision.collider.tag == "Coin")
+            {
+                Vector2 contactPoint = collision.contacts[0].point;
+                transform.position = new Vector2(85.0f, 0.0f);
+            }
+
+            livesValue = 3;
+            lives.text = livesValue.ToString();
+        }
+
+        if (scoreValue >= 8)
         {
             winText.SetActive(true);
+
+            if (!gameOver)
+            {
+                musicSource.clip = backgroundMusic;
+                musicSource.Stop();
+                musicSource.loop = false;
+                musicSource.PlayOneShot(winMusic);
+                gameOver = true;
+            }
+        }
+
+        if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            lives.text = livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+
+        if (livesValue <= 0)
+        {
+            loseText.SetActive(true);
+            AudioSource.PlayClipAtPoint(loseMusic, transform.position);
+            gameObject.SetActive(false);
         }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Ground")
+        if (collision.collider.tag == "Ground" && isOnGround)
         {
             if (Input.GetKey(KeyCode.W))
             {
